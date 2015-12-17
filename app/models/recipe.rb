5e1facdf9b0ff171e8a84d970b00b383a521549b
John McIntosh II <jmci10@hotmail.com>
2015-12-17 09:13:57 +0000

    configure paperclip gem

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+    
+    has_attached_file :image, styles: { medium: "350x350#" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
